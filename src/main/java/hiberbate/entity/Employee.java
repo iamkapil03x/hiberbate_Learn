e0c5af7e44984b792bbfe1319634d6d4dfffdc54
iamkapil03x <112274111+iamkapil03x@users.noreply.github.com>
2024-01-14 23:38:11 +0530

    xml Baased
    Hibernate allows you to work with persistent XML data in much the same way you work with persistent POJOs. A parsed XML tree can be thought of as another way of representing the relational data at the object level, instead of POJOs. Hibernate supports dom4j as API for manipulating XML trees.

--- src/main/java/hiberbate/entity/Employee.java
+++ src/main/java/hiberbate/entity/Employee.java
@@ -3,7 +3,9 @@ package hiberbate.entity;
 public class Employee {
     private String name;
     private String gender;
+    private  int salary;
     private int id;
+    private String address;
 
     public String getName() {
         return name;
@@ -28,13 +30,30 @@ public class Employee {
     public void setId(int id) {
         this.id = id;
     }
+    public int getSalary(){
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     @Override
     public String toString() {
         return "Employee{" +
                 "name='" + name + '\'' +
                 ", gender='" + gender + '\'' +
+                ", salary=" + salary +
                 ", id=" + id +
+                ", address =" +address+
                 '}';
     }
 }
